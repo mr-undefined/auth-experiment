714d7fd35534976617e95467572ef6f8b584c8b2
Oleksandr Savchuk <osavchuk@kealabs.com>
2021-03-27 02:54:10 +0200

    move constants to constants.js

--- server/services/AuthService.js
+++ server/services/AuthService.js
@@ -2,25 +2,17 @@
 // import { v4 as uuid4 } from 'uuid';
 import User from '../models/User';
 import logger from '../utils/logger';
-
-// TODO: Move it to config or utils.
-const DATA_IS_INVALID = 'DATA_IS_INVALID';
-const EMAIL_IS_NOT_VALID = 'EMAIL_IS_NOT_VALID';
-const PASSWORD_IS_TOO_SHORT = 'PASSWORD_IS_TOO_SHORT';
-const NAME_IS_NOT_VALID = 'NAME_IS_NOT_VALID';
-const USER_ALREADY_CREATED = 'USER_ALREADY_CREATED';
-const USER_NOT_FOUND = 'USER_NOT_FOUND';
-const BAD_UUID = 'BAD_UUID';
+import * as c from '../utils/constants';
 
 const AuthService = function () {
   this.createUser = (user) => {
     logger.info('Try to create user with next data : ', user);
-    if (!user) throw new Error(DATA_IS_INVALID);
+    if (!user) throw new Error(c.DATA_IS_INVALID);
     const { email, password, name } = user;
-    if (!email) throw new Error(EMAIL_IS_NOT_VALID);
-    if (User.getByEmail(email)) throw new Error(USER_ALREADY_CREATED);
-    if (!password || password.length < 4) throw new Error(PASSWORD_IS_TOO_SHORT);
-    if (!name) throw new Error(NAME_IS_NOT_VALID);
+    if (!email) throw new Error(c.EMAIL_IS_NOT_VALID);
+    if (User.getByEmail(email)) throw new Error(c.USER_ALREADY_CREATED);
+    if (!password || password.length < 4) throw new Error(c.PASSWORD_IS_TOO_SHORT);
+    if (!name) throw new Error(c.NAME_IS_NOT_VALID);
 
     // const uuid = uuid4(); // TODO: Uncomment before deploy.
     const uuid = '7ff7c131-0e76-4d50-bd1c-54027d3f2e5f';
@@ -33,9 +25,9 @@ const AuthService = function () {
 
   this.getUser = (uuid) => {
     logger.trace('Try to find user with uuid -', uuid);
-    if (!uuid) throw new Error(BAD_UUID);
+    if (!uuid) throw new Error(c.BAD_UUID);
     const user = User.getByUUID(uuid);
-    if (!user) throw new Error(USER_NOT_FOUND);
+    if (!user) throw new Error(c.USER_NOT_FOUND);
     return user;
   };
 };
